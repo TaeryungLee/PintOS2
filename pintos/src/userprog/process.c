dd5fd236a1ec9d80cd2817498c96a82e9478ad4a
TaeryungLee Mac <eoyoung2@kaist.ac.kr>
2020-10-09 02:14:02 +0900

    2.1 continued

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -21,50 +21,128 @@
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp);
 
+
 /* Starts a new thread running a user program loaded from
    FILENAME.  The new thread may be scheduled (and may even exit)
    before process_execute() returns.  Returns the new process's
    thread id, or TID_ERROR if the thread cannot be created. */
+   /*Modified 2.1*/
 tid_t
 process_execute (const char *file_name) 
 {
   char *fn_copy;
   tid_t tid;
-
+  char program[64];
+  int file_name_length = strlen(file_name)+1;
+  char *save_ptr=NULL;
+  char *token;
   /* Make a copy of FILE_NAME.
      Otherwise there's a race between the caller and load(). */
   fn_copy = palloc_get_page (0);
   if (fn_copy == NULL)
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
+  printf("break");
+  strlcpy(program, file_name,file_name_length); //modified
+
+  token = strtok_r(program, " ", &save_ptr); //tokenize
 
   /* Create a new thread to execute FILE_NAME. */
-  tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
+  tid = thread_create (token, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy); 
   return tid;
+  //free(program);
 }
 
+/* Modified 2.1 */
+  void argument_stack(char **parse, int count, void **esp)
+  {
+    unsigned int arg_addr[count-1]; // argv의 주소값을 저장할 변수
+
+    /*argument push*/
+    int i, j;
+    for(i = count-1; i > -1; i--)
+    {
+      for(j = strlen(parse[i]); j > -1; j--)
+      {
+        *esp -= 1;
+        **(char **)esp = parse[i][j];
+      }
+      printf("%x", *(int*)*esp);
+      arg_addr[i] = *(int *) *esp;/* esp현재위치를 arg_addr에 저장*/
+    }
+
+    /* word-align */
+    int addr = *(int *)*esp;
+    for(i=0; i < 4 - (addr%4); i++)
+    {
+      *esp -= 1;
+      *(int*)(*esp) = 0;
+    }
+
+    *esp -= 4;
+    *(int *)(*esp) = 0;
+
+    /* argv push */
+    for(i=count-1; i >-1; i--)
+    {
+      *esp -=4;
+      *(void **)(*esp) = arg_addr[i]; /*arg_addr에 저장된 주소값을 현재 esp의 값으로 저장*/
+    }
+
+    /* argc push */
+    *esp -= 4;
+    *(int *)(*esp) = count;
+
+    /*fake address push */
+    *esp -= 4;
+    *(int*)(*esp) = 0;
+  }
+  
 /* A thread function that loads a user process and starts it
    running. */
+/* Modified 2.1 */
 static void
 start_process (void *file_name_)
 {
   char *file_name = file_name_;
   struct intr_frame if_;
   bool success;
+  char *save_ptr=NULL;
+  char *token;
+  int count = 0;
+  char *parse[64];
 
+  /*fn_copy = palloc_get_page (0);
+  if (fn_copy == NULL)
+    return TID_ERROR;
+  strlcpy (fn_copy, file_name, PGSIZE);
+  printf("break");
+  token = strtok_r(fn_copy, " ", &save_ptr); //tokenize
+  */
+
+  for(token = strtok_r(*file_name, " ", &save_ptr); token != NULL; token = strtok_r(NULL, " ", &save_ptr))
+  {
+    parse[count] = token;
+    printf("%c", &parse[count]);
+    count ++;
+  }
+  printf("%d", count);
   /* Initialize interrupt frame and load executable. */
   memset (&if_, 0, sizeof if_);
   if_.gs = if_.fs = if_.es = if_.ds = if_.ss = SEL_UDSEG;
   if_.cs = SEL_UCSEG;
   if_.eflags = FLAG_IF | FLAG_MBS;
-  success = load (file_name, &if_.eip, &if_.esp);
+  success = load (parse[0], &if_.eip, &if_.esp);
 
+  argument_stack(parse, count, &if_.esp);
+  hex_dump(if_.esp, if_.esp, PHYS_BASE - if_.esp, true);
   /* If load failed, quit. */
   palloc_free_page (file_name);
   if (!success) 
     thread_exit ();
+
 
   /* Start the user process by simulating a return from an
      interrupt, implemented by intr_exit (in
@@ -74,6 +152,8 @@ start_process (void *file_name_)
      and jump to it. */
   asm volatile ("movl %0, %%esp; jmp intr_exit" : : "g" (&if_) : "memory");
   NOT_REACHED ();
+
+  free(parse);
 }
 
 /* Waits for thread TID to die and returns its exit status.  If
@@ -88,7 +168,10 @@ start_process (void *file_name_)
 int
 process_wait (tid_t child_tid UNUSED) 
 {
-  return -1;
+  while(1)
+  {
+
+  }
 }
 
 /* Free the current process's resources. */
