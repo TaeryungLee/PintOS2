ffe152f0d37b88a515fa034ca5a9944b6af70ddd
Seungho <anubis9405@gmail.com>
2020-10-09 05:05:17 +0900

    argument passing 2.1 ongoing

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -20,7 +20,7 @@
 
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp);
-void argument_stack(cahr **parse, int count, void **esp);
+void argument_stack(char **parse, int count, void **esp);
 
 /* Starts a new thread running a user program loaded from
    FILENAME.  The new thread may be scheduled (and may even exit)
