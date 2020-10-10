3b2dca4e5c04f4886db8dcc5ef12e946485e8e24
TaeryungLee Mac <eoyoung2@kaist.ac.kr>
2020-10-10 09:57:30 +0900

    2.1 done

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -83,7 +83,7 @@ process_execute (const char *file_name)
     //hex_dump(*esp, *esp, PHYS_BASE - *esp, true);
 
     /* word-align */
-    int addr = (int) *esp;
+    int addr = - (int) *esp;
     printf("%#x \n", addr);
     for(i=0; i < (addr%4); i++)
     {
