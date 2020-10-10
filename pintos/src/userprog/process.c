4a546969eaf65ff7f551739f78ce4654d24bd4c7
TaeryungLee Mac <eoyoung2@kaist.ac.kr>
2020-10-10 10:00:12 +0900

    2.1 done

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -85,7 +85,7 @@ process_execute (const char *file_name)
     /* word-align */
     int addr = - (int) *esp;
     printf("%#x \n", addr);
-    for(i=0; i < (addr%4); i++)
+    for(i=0; i < ((addr + 2)%4); i++)
     {
       *esp -= 1;
       *(char *)(*esp) = 0;
