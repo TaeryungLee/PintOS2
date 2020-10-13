da1ac1d663715fc21676448be499ad25eeda3608
TaeryungLee Mac <eoyoung2@kaist.ac.kr>
2020-10-13 13:27:13 +0900

    2.4

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -198,11 +198,11 @@ int
 process_wait (tid_t child_tid) 
 {
   struct thread *child = get_child(child_tid);
-  printf("11 %d", child);
   if (child == NULL)
     return -1;
   int exit_status = child->exit_status;
   sema_down(&child->exit_sema);
+  remove_child(child_tid);
 
   if (exit_status < 0)
     return exit_status;
