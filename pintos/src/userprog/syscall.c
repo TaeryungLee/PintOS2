e0f135cab015415e42d4e7b9edc72871c90ed3f2
TaeryungLee Mac <eoyoung2@kaist.ac.kr>
2020-10-13 11:36:07 +0900

    2.4

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -66,7 +66,7 @@ syscall_handler (struct intr_frame *f)
   void *esp = f->esp;
   // Check if esp is valid
   check(esp, 4);
-  printf("%#x \n", esp);
+  printf("%#x, %#x \n", esp, &esp);
   bool res = check_byte(esp);
   printf("%d \n", res);
   printf("%d \n", is_user_vaddr(&esp));
