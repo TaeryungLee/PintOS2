3deca8e37942c93c80bc1fa95d5623c019ee3a37
TaeryungLee Mac <eoyoung2@kaist.ac.kr>
2020-10-19 01:14:10 +0900

    2.4

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -62,16 +62,17 @@ process_execute (const char *file_name)
 
   struct thread *child = get_child(tid);
 
-  if (child->exit_status == -1)
-  {
-    process_wait(tid);
-  }
-  //for (e = list_begin(&thread_current()->children); e != list_end(&thread_current()->children); e = list_next(e))
+  //if (child->exit_status == -1)
   //{
-  //  struct thread *iter = list_entry(e, struct thread, child_elem);
-  //  if (iter->exit_status == -1)
-  //    return process_wait(tid);
+  //  process_wait(tid);
   //}
+
+  for (e = list_begin(&thread_current()->children); e != list_end(&thread_current()->children); e = list_next(e))
+  {
+    struct thread *iter = list_entry(e, struct thread, child_elem);
+    if (iter->exit_status == -1)
+      return process_wait(tid);
+  }
   return tid;
   //free(program);
 }
