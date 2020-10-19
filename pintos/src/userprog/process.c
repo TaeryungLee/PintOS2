eec16de8726da5797caf107466a3464461eb1a0e
TaeryungLee Mac <eoyoung2@kaist.ac.kr>
2020-10-19 21:09:54 +0900

    oom

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -178,7 +178,7 @@ start_process (void *file_name_)
   if_.cs = SEL_UCSEG;
   if_.eflags = FLAG_IF | FLAG_MBS;
   success = load (parse[0], &if_.eip, &if_.esp);
-
+  printf("reached2\n");
   /* If load failed, quit. */
   if (!success)
   {
