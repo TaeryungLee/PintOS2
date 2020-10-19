edbb823053f6ba279743206aec7a20dce51ccf89
TaeryungLee Mac <eoyoung2@kaist.ac.kr>
2020-10-19 19:02:34 +0900

    oom

--- pintos/src/tests/userprog/no-vm/multi-oom.c
+++ pintos/src/tests/userprog/no-vm/multi-oom.c
@@ -150,8 +150,9 @@ main (int argc, char *argv[])
 
       /* Else wait for child to report how deeply it was able to recurse. */
       int reached_depth = wait (child_pid);
-      //if (reached_depth == -1)
-      //  fail ("wait returned -1.");
+      msg("%d", reached_depth);
+      if (reached_depth == -1)
+        fail ("wait returned -1.");
 
       /* Record the depth reached during the first run; on subsequent
          runs, fail if those runs do not match the depth achieved on the
