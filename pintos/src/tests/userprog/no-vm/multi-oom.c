7129ae3f8e7cce8f76d1b360db7c34ea902adb8a
TaeryungLee Mac <eoyoung2@kaist.ac.kr>
2020-10-19 22:13:07 +0900

    oom

--- pintos/src/tests/userprog/no-vm/multi-oom.c
+++ pintos/src/tests/userprog/no-vm/multi-oom.c
@@ -24,8 +24,8 @@
 #include <random.h>
 #include "tests/lib.h"
 
-static const int EXPECTED_DEPTH_TO_PASS = 12;
-static const int EXPECTED_REPETITIONS = 6;
+static const int EXPECTED_DEPTH_TO_PASS = 6;
+static const int EXPECTED_REPETITIONS = 2;
 
 enum child_termination_mode { RECURSE, CRASH };
 
