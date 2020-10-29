22a5dc2451376d2067f8c837364151bf6c92015c
TaeryungLee Mac <eoyoung2@kaist.ac.kr>
2020-10-29 21:59:13 +0900

    3-1.1 debug: includes

--- pintos/src/vm/page.c
+++ pintos/src/vm/page.c
@@ -9,6 +9,8 @@
 #include <list.h>
 #include <hash.h>
 
+#define VM
+
 unsigned vm_hash_func (const struct hash_elem *e, void *aux);
 bool vm_less_func (const struct hash_elem *a, const struct hash_elem *b, void *aux);
 
