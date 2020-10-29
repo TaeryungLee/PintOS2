605ce8b873268db05d7c83378de0cede6a110980
TaeryungLee Mac <eoyoung2@kaist.ac.kr>
2020-10-30 04:32:44 +0900

    3-1.1 debug: includes

--- pintos/src/vm/page.c
+++ pintos/src/vm/page.c
@@ -97,16 +97,16 @@ bool delete_vme (struct hash *vm, struct vm_entry *vme)
 struct vm_entry *find_vme(void *vaddr)
 {
 	void* page;
-	struct vm_entry vme;
+	struct vm_entry *vme;
 	struct vm_entry *vme_found;
 	struct hash_elem *elem;
 	struct thread *cur = thread_current();
 
 	page = pg_round_down(vaddr);
 
-	vme->vaddr = page;
+	vme.vaddr = page;
 	//printf("%d, %d, %d, %d\n", vme, &vme, vme->elem, &vme->elem);
-	elem = hash_find(&cur->vm, &vme->elem);
+	elem = hash_find(&cur->vm, &vme.elem);
 
 	if (elem == NULL)
 		return NULL;
