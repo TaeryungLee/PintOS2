889d2885add40f93e20d17226735d4b5795b8708
TaeryungLee Mac <eoyoung2@kaist.ac.kr>
2020-10-30 04:35:10 +0900

    3-1.1 debug: includes

--- pintos/src/vm/page.c
+++ pintos/src/vm/page.c
@@ -107,7 +107,7 @@ struct vm_entry *find_vme(void *vaddr)
 
 	vme->vaddr = page;
 	//printf("%d, %d, %d, %d\n", vme, &vme, vme->elem, &vme->elem);
-	elem = hash_find(&cur->vm, &vme.elem);
+	elem = hash_find(&cur->vm, &vme->elem);
 
 	if (elem == NULL)
 		return NULL;
