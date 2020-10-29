a4cbf885f3cd919b106a1f0a02760d4d926e6a06
TaeryungLee Mac <eoyoung2@kaist.ac.kr>
2020-10-30 05:09:07 +0900

    3-1.1 debug: includes

--- pintos/src/vm/page.c
+++ pintos/src/vm/page.c
@@ -178,7 +178,7 @@ bool load_file (void* kaddr, struct vm_entry *vme)
 	printf("%#x\n", kaddr);
 	int read_bytes = file_read_at(vme->file, kaddr, vme->read_bytes, vme->offset);
 
-	printf("rb %d, %d\n", vme->read_bytes, read_bytes);
+	printf("rb %d, %d, %d\n", vme->read_bytes, read_bytes, vme->offset);
 	// if read fails, return false
 	if ((int)vme->read_bytes != read_bytes)
 		return false;
