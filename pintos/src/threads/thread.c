b33093bb05b0fd7971be4003d1611d1da731185a
TaeryungLee Mac <eoyoung2@kaist.ac.kr>
2020-10-29 21:36:40 +0900

    3-1.1 debug: includes

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -11,9 +11,7 @@
 #include "threads/switch.h"
 #include "threads/synch.h"
 #include "threads/vaddr.h"
-#ifdef USERPROG
 #include "userprog/process.h"
-#endif
 #include "vm/page.h"
 
 /* Random value for struct thread's `magic' member.
