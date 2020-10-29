3d2cc0d01936f19070157437416c115e73a41ccf
TaeryungLee Mac <eoyoung2@kaist.ac.kr>
2020-10-29 17:08:04 +0900

    3-1.1 debug

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -178,7 +178,7 @@ start_process (void *file_name_)
 
   // Modified 3-1.1
   // use vm_init() to initialize hash table
-  vm_init(new->vm);
+  vm_init(&new->vm);
 
   /* Initialize interrupt frame and load executable. */
   memset (&if_, 0, sizeof if_);
@@ -708,7 +708,7 @@ setup_stack (void **esp)
         // add into hash table
         struct thread *cur = thread_current();
         insert_vme(&cur->vm, vme);
-        
+
         *esp = PHYS_BASE;
       }
       else
