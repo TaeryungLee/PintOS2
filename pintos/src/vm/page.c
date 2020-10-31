21387540dcbd4929fa551d41634541a2a2d8907f
TaeryungLee Mac <eoyoung2@kaist.ac.kr>
2020-10-31 22:54:06 +0900

    3-1.1 debug: sync

--- pintos/src/vm/page.c
+++ pintos/src/vm/page.c
@@ -157,10 +157,10 @@ bool load_file (void* kaddr, struct vm_entry *vme)
 	// try to read from file
 	//int read_bytes = file_read_at(vme->file, kaddr, vme->read_bytes, vme->offset);
 
-	file_seek(vme->file, vme->offset);
+	// file_seek(vme->file, vme->offset);
 	int read_bytes2 = file_read(vme->file, kaddr, vme->read_bytes);
 	//printf("%d %d %d %d\n", vme->read_bytes, read_bytes, read_bytes2, vme->offset);
-
+	printf("%d %d %d\n", vme->read_bytes, read_bytes2, vme->offset);
 	// if read fails, return false
 	if ((int)vme->read_bytes != read_bytes2)
 		return false;
