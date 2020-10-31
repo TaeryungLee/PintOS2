87c4636c898a61eb85f0477b69db59e6ae233978
TaeryungLee Mac <eoyoung2@kaist.ac.kr>
2020-10-31 22:50:51 +0900

    3-1.1 debug: sync

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -667,7 +667,7 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
   // Modified 3.1-1
   struct file* reopen = file_reopen(file);
 
-  file_seek (file, ofs);
+  file_seek (reopen, ofs);
 
   //debug
   if (reopen == NULL)
