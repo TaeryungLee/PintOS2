751f442d4017d2b465227a024be693cc9131e35a
TaeryungLee Mac <eoyoung2@kaist.ac.kr>
2020-11-01 01:36:43 +0900

    3-1.1 debug: sync, memory lock mod

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -314,6 +314,7 @@ thread_current (void)
      have overflowed its stack.  Each thread has less than 4 kB
      of stack, so a few big automatic arrays or moderate
      recursion can cause stack overflow. */
+  printf("name %s\n", t->name);
   ASSERT (is_thread (t));
   ASSERT (t->status == THREAD_RUNNING);
 
