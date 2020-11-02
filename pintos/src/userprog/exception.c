e4b5d79ac98d9a8250189986ee7994fe6c7df728
TaeryungLee Mac <eoyoung2@kaist.ac.kr>
2020-11-03 06:44:17 +0900

    3-1.2 debug: page-merge-stk

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -171,9 +171,10 @@ page_fault (struct intr_frame *f)
     {
       printf("verify fucked\n");
       //printf("addr %#x %#x\n", fault_addr, f->esp);
-      printf("%d %d %d\n", (int32_t)PHYS_BASE, (int32_t)fault_addr, (int32_t)PHYS_BASE - (int32_t)fault_addr);
-      printf("%d %d \n", !(f->esp > 0xc0000000), 8 * 1024 * 1024 < (int32_t)PHYS_BASE - (int32_t)fault_addr);
-      if (!(f->esp > 0xc0000000) && (8 * 1024 * 1024 < (int32_t)PHYS_BASE - (int32_t)fault_addr))
+      printf("%#x %#x %d\n", 
+        (unsigned int)PHYS_BASE, (unsigned int)fault_addr, (unsigned int)PHYS_BASE - (unsigned int)fault_addr);
+      printf("%d %d \n", !(f->esp > 0xc0000000), 8 * 1024 * 1024 < (unsigned int)PHYS_BASE - (unsigned int)fault_addr);
+      if (!(f->esp > 0xc0000000) && (8 * 1024 * 1024 < (unsigned int)PHYS_BASE - (unsigned int)fault_addr))
       {
         exits(-1, NULL);
       }
