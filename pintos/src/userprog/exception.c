38ec89a96e3fcfb248749e00a59f14fa803207f1
TaeryungLee Mac <eoyoung2@kaist.ac.kr>
2020-11-02 19:30:18 +0900

    3-1.2 debug: refresh, syscall remains

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -180,9 +180,9 @@ page_fault (struct intr_frame *f)
     vme = find_vme(fault_addr);
     if (vme == NULL)
       printf("still no vme\n");
-    return;
+    
   }
-
+  printf("%d %d %d\n", not_present, write, user);
   bool load_succ = handle_mm_fault(vme);
 
   if (!load_succ)
