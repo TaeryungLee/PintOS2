3180e242fde42b2609a03d35f2d710615d1e109d
TaeryungLee Mac <eoyoung2@kaist.ac.kr>
2020-11-03 11:59:26 +0900

    3-2 debug

--- pintos/src/lib/string.c
+++ pintos/src/lib/string.c
@@ -57,8 +57,6 @@ memcmp (const void *a_, const void *b_, size_t size)
 
   ASSERT (a != NULL || size == 0);
   ASSERT (b != NULL || size == 0);
-  printf("%s", &a_);
-  printf("%s", &b_);
 
   for (; size-- > 0; a++, b++)
     if (*a != *b)
