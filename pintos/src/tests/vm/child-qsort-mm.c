9a0fbf6c204cf4e6c2ca641db9c3c174e63d8bda
TaeryungLee Mac <eoyoung2@kaist.ac.kr>
2020-11-03 14:18:12 +0900

    3-2 debug

--- pintos/src/tests/vm/child-qsort-mm.c
+++ pintos/src/tests/vm/child-qsort-mm.c
@@ -14,7 +14,7 @@ main (int argc UNUSED, char *argv[])
   unsigned char *p = (unsigned char *) 0x10000000;
 
   test_name = "child-qsort-mm";
-  quiet = false;
+  quiet = true;
 
   CHECK ((handle = open (argv[1])) > 1, "open \"%s\"", argv[1]);
   CHECK (mmap (handle, p) != MAP_FAILED, "mmap \"%s\"", argv[1]);
