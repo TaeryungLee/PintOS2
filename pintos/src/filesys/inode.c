083fe8467d585374d864f0850166669ddf3b907a
Seungho <anubis9405@gmail.com>
2020-12-05 20:33:38 +0900

    ongoing 4-2

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -223,7 +223,8 @@ inode_read_at (struct inode *inode, void *buffer_, off_t size, off_t offset)
       if (sector_ofs == 0 && chunk_size == BLOCK_SECTOR_SIZE)
         {
           /* Read full sector directly into caller's buffer. */
-          block_read (fs_device, sector_idx, buffer + bytes_read);
+          //block_read (fs_device, sector_idx, buffer + bytes_read);
+          bc_read(sector_idx, buffer, bytes_read, chunk_size, sector_ofs);
         }
       else 
         {
@@ -284,7 +285,8 @@ inode_write_at (struct inode *inode, const void *buffer_, off_t size,
       if (sector_ofs == 0 && chunk_size == BLOCK_SECTOR_SIZE)
         {
           /* Write full sector directly to disk. */
-          block_write (fs_device, sector_idx, buffer + bytes_written);
+          //block_write (fs_device, sector_idx, buffer + bytes_written);
+          bc_write(sector_idx, buffer, bytes_written, chunk_size, sector_ofs);
         }
       else 
         {
