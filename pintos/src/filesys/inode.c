43d4c033fe92c6673e96e0ea98e731e049b2e6b6
Seungho <anubis9405@gmail.com>
2020-12-07 22:48:54 +0900

    4-2 debug

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -182,7 +182,7 @@ inode_create (block_sector_t sector, off_t length)
       size_t sectors = bytes_to_sectors (length);
       disk_inode->length = 0;
       disk_inode->magic = INODE_MAGIC;
-      printf("inode_length: %f \n", disk_inode->length);
+      printf("inode_length: %d \n", disk_inode->length);
       //modified 4-2
       if(length > 0)
       {
@@ -588,7 +588,7 @@ bool inode_update_file_length(struct inode_disk *inode_disk, off_t start_pos, of
   {
     return false;
   }
-  printf("size:%f  size_old:%f", size, size_old);
+  printf("size:%d  size_old:%d", size, size_old);
   if(size_old < size)
   {
     return false;
