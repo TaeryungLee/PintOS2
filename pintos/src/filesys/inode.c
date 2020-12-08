505127fa888bd5131cf3d391413dbd80fb1dc2cf
Seungho <anubis9405@gmail.com>
2020-12-08 17:53:23 +0900

    4-2 debug

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -544,7 +544,10 @@ static bool register_sector(struct inode_disk *inode_disk, block_sector_t new_se
       new_block = malloc(sizeof (struct inode_indirect_block));
       if(inode_disk->indirect_block_sec == error)
       {
-        free_map_allocate(1, inode_disk->indirect_block_sec);
+        if(free_map_allocate(1, inode_disk->indirect_block_sec) == false)
+        {
+          return false;
+        }
       }
       bc_read(inode_disk->indirect_block_sec, new_block, 0, sizeof(struct inode_indirect_block), 0);
       new_block->map_table[sec_loc.index1] = new_sector;
@@ -559,7 +562,10 @@ static bool register_sector(struct inode_disk *inode_disk, block_sector_t new_se
       new_block = malloc(sizeof (struct inode_indirect_block));
       if(inode_disk->double_indirect_block_sec == error)
       {
-        free_map_allocate(1, inode_disk->double_indirect_block_sec);    
+        if(free_map_allocate(1, inode_disk->double_indirect_block_sec) == false)
+        {
+          return false;
+        }
       }
       bc_read(inode_disk->double_indirect_block_sec, new_block, 0, sizeof(struct inode_indirect_block), 0);
       temp_sec = new_block->map_table[sec_loc.index2];
