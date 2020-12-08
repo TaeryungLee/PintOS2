7dc51cfd7f1a0da72285c3fbcfe13daad95e22f5
Seungho <anubis9405@gmail.com>
2020-12-09 08:28:32 +0900

    4-2 debug

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -359,10 +359,9 @@ inode_read_at (struct inode *inode, void *buffer_, off_t size, off_t offset)
 {
   uint8_t *buffer = buffer_;
   off_t bytes_read = 0;
-  uint8_t *bounce = NULL;
 
   //modified 4-2
-  struct inode_disk *inode_disk = malloc(sizeof (struct inode_disk));
+  struct inode_disk *inode_disk;// = malloc(sizeof (struct inode_disk));
   //printf("%d", sizeof(struct inode_disk));
   lock_acquire(&inode->extend_lock);
   get_disk_inode(inode, inode_disk);
