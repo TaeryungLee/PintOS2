ac14fd115e2ba67a93777e0833ae1f430cd695b7
Seungho <anubis9405@gmail.com>
2020-12-09 20:26:21 +0900

    merge

--- pintos/src/filesys/buffer_cache.c
+++ pintos/src/filesys/buffer_cache.c
@@ -100,9 +100,9 @@ struct buffer_head *bc_select_victim(void)
             lock_acquire(&ch->lock);
             if(ch->clock_bit == false)
             {
+                ch++;
                 if(ch->dirty_flag == true)
                 {
-                    ch++;
                     block_write(fs_device, ch->sector_addr, ch->buffer);
                     return ch;
                 }
