919182ba09424568492081512b35630dcc79b0c5
Seungho <anubis9405@gmail.com>
2020-12-14 08:06:22 +0900

    4.2 write behind

--- pintos/src/filesys/filesys.c
+++ pintos/src/filesys/filesys.c
@@ -180,6 +180,11 @@ struct dir* parse_path(char *path_name, char *file_name)
 
   token = strtok_r(path_name, "/", &save_ptr);
   next_token = strtok_r(NULL, "/", &save_ptr);
+  if (token == NULL)
+  {
+    strlcpy (file_name, ".", 512);
+    return dir;
+  }
   while(token != NULL && next_token != NULL)
   {
 
