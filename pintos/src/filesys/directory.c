ed66da5919a50ab7c88119b756fc79a4a25efd73
Seungho <anubis9405@gmail.com>
2020-12-15 15:12:01 +0900

    4.3 debug

--- pintos/src/filesys/directory.c
+++ pintos/src/filesys/directory.c
@@ -199,7 +199,7 @@ dir_remove (struct dir *dir, const char *name)
 
   ASSERT (dir != NULL);
   ASSERT (name != NULL);
-  
+
   if(!strcmp(name, ".") || !strcmp(name, ".."))
   {
     return false;
@@ -239,16 +239,12 @@ dir_readdir (struct dir *dir, char name[NAME_MAX + 1])
   while (inode_read_at (dir->inode, &e, sizeof e, dir->pos) == sizeof e) 
     {
       dir->pos += sizeof e;
-      if (e.in_use)
+      if (e.in_use && strcmp(e.name, ".") && strcmp(e.name,"."))
         {
-          if(strcmp(e.name, "."))
-          {
-            if(strcmp(e.name, ".."))
-            {
+
               strlcpy (name, e.name, NAME_MAX + 1);
               return true;
-            }
-          }
+
 
         } 
     }
--- pintos/src/filesys/filesys.c
+++ pintos/src/filesys/filesys.c
@@ -244,20 +244,10 @@ struct dir* parse_path(char *path_name, char *file_name)
     {
       dir_close(dir);
       return NULL;
-    }*//*
+    }*/
     if(dir_lookup(dir, token, &inode) == false || inode_is_dir(inode) == false)
     {
       dir_close(dir);
-      return NULL;
-    }*/
-    if (!dir_lookup (dir, token, &inode))
-    {
-      dir_close (dir);
-      return NULL;
-    }
-    if (!inode_is_dir (inode))
-    {
-      dir_close (dir);
       return NULL;
     }
     dir_close(dir);
