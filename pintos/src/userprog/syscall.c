4aa3265369759aab493e953b346b1590b0fa7a24
Seungho <anubis9405@gmail.com>
2020-12-17 10:00:44 +0900

    4.3 debug

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -894,7 +894,7 @@ bool chdir (const char *name)
 {
   char cp_name[PATH_MAX_LEN + 1];
   strlcpy (cp_name, name, PATH_MAX_LEN);
-  //strlcat (cp_name, "/0", PATH_MAX_LEN);
+  strlcat (cp_name, "/0", PATH_MAX_LEN);
 
   char file_name[PATH_MAX_LEN + 1];
   struct inode *target_inode = dir_get_inode(parse_path (cp_name, file_name));
